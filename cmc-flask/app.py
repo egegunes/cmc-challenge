942192741f314d82e24a56edb200356d05f371be
Ege Güneş <egegunes@gmail.com>
2018-11-28 23:35:42 +0300

    [cmc-flask] Add prometheus metrics

--- cmc-flask/app.py
+++ cmc-flask/app.py
@@ -1,6 +1,7 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from prometheus_client import Summary, generate_latest, CONTENT_TYPE_LATEST
 
 from models import db, Currency
 
@@ -17,8 +18,16 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://{USER}:{PASSWORD}@{HOST}/{
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
+response_time = Summary('response_time_seconds', 'Response time in seconds')
+
+
+@app.route("/metrics")
+def metrics():
+    return Response(generate_latest(), mimetype=CONTENT_TYPE_LATEST)
+
 
 @app.route("/")
+@response_time.time()
 def get_prices():
     currencies = Currency.query.distinct(Currency.symbol)\
                                .order_by(Currency.symbol, Currency.time.desc())
